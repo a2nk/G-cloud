cbe40546fc51c14894bc44a4dbdbf479dcd9e99b
Aank <31814027+a2nk@users.noreply.github.com>
2022-01-09 21:13:24 +0700

    Create xrdp.sh

---
+++ xrdp.sh
@@ -0,0 +1,48 @@
+apt update
+apt install screen -y
+apt install sudo -y
+sudo adduser aank --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "aank:LinggaHosting" | sudo chpasswd
+sudo usermod -aG sudo,adm aank
+clear
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/GCngr0k > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+nohup ./ngrok tcp --region $CRP 3389 &>/dev/null &
+clear
+echo "===================================="
+echo "Install XFCE"
+echo "===================================="
+sudo apt install -y xfce4 xfce4-goodies > /dev/null 2>&1
+echo "=======25%"
+sudo apt install firefox -y > /dev/null 2>&1
+echo "=============50%"
+sudo apt-get install -y xrdp > /dev/null 2>&1
+sudo apt-get install -y xfce4-terminal
+echo "=================70%"
+echo xfce4-session >~/.xsession
+echo "=====================90%"
+sudo service xrdp start
+clear
+echo "=======================100%"
+echo "===================================="
+echo "Username : aank"
+echo "Password : LinggaHosting"
+echo "right click to open menu"
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+rm -rf ngrok.zip xrdp.sh
+sleep 43210
