8a3cf5df1abf55eb8c55cdde0961db603b0d3c56
Aank <31814027+a2nk@users.noreply.github.com>
2022-01-09 23:05:15 +0700

    Create goormIDE.sh

---
+++ goormIDE.sh
@@ -0,0 +1,26 @@
+sudo adduser akuhnet --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "aank:LinggaHosting" | sudo chpasswd
+sudo usermod -aG sudo,adm aank
+apt update
+clear
+echo "===================================="
+echo "Install XFCE"
+echo "Choose Keyboard 31 then 1"
+echo "===================================="
+sudo apt install -y xfce4 xfce4-goodies > /dev/null 2>&1
+echo "=======25%"
+sudo apt install firefox -y > /dev/null 2>&1
+echo "=============50%"
+sudo apt-get install -y xrdp > /dev/null 2>&1
+sudo apt-get install -y xfce4-terminal > /dev/null 2>&1
+echo "=================70%"
+echo xfce4-session >~/.xsession
+echo "=====================90%"
+sudo service xrdp start
+clear
+echo "=======================100%"
+echo "===================================="
+echo "Username : aank"
+echo "Password : LinggaHosting"
+echo Go to settings add port 3389
+echo "===================================="
