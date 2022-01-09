6ae41e912ce1f17a39d7b511f861dd0493735a33
Aank <31814027+a2nk@users.noreply.github.com>
2022-01-09 18:56:17 +0700

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,38 @@
+echo -e "$123\n$123\n" | sudo passwd
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://ngrok > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 3388 &>/dev/null &
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+docker pull danielguerra/ubuntu-xrdp
+clear
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+echo "===================================="
+echo "Username : ubuntu"
+echo "Password : ubuntu"
+echo "RDP Address:"
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "===================================="
+echo "Don't close this tab to keep RDP running"
+echo "Wait to finish bot and next open RDC to connect"
+echo "===================================="
+echo "===================================="
+docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:20.04 > /dev/null 2>&1
