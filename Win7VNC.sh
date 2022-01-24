465611c9173d2405b2a8fc518d8c13ba3ff2b157
Aank <31814027+a2nk@users.noreply.github.com>
2022-01-24 12:20:56 +0700

    Create Win7VNC.sh

---
+++ Win7VNC.sh
@@ -0,0 +1,20 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 5900 &>/dev/null &
+echo Please wait for installing...
+sudo apt update -y > /dev/null 2>&1
+echo "Installing QEMU (2-3m)..."
+sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
+echo Downloading Windows files from aank.me
+curl -L -o win7.img https://go.aank.me/win/win7lite.img
+echo "Windows 7 x86 Lite On Gitpod, GCloud and GColab"
+echo Your VNC IP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "Note: Use Right-Click Or Ctrl+C To Copy"
+echo "Please Keep this Tab to Open, Maximum Time 50h"
+echo "Support YT Channel-> Aank is ME"
+echo "LinK: https://aank.me/Youtube"
+sudo qemu-system-x86_64 -vnc :0 -hda win7.img  -smp cores=2  -m 8192M -net user,hostfwd=tcp::3388-:3389 -machine usb=on -device usb-tablet > /dev/null 2>&1
+sleep 43210
