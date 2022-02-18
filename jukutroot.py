95a65872e3057930f02d37acc1c3e08b48015722
Aank <31814027+a2nk@users.noreply.github.com>
2022-02-18 17:31:13 +0700

    Create jukutroot.py

---
+++ jukutroot.py
@@ -0,0 +1,5 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+gdd.download_file_from_google_drive(file_id='17-1MDyPJmW96StdgVH8zB4AkQYUtJ9Bd',
+                                    dest_path='./a2nkroot.zip',
+                                    unzip=True)
