849f2550ccba6767b8accd4f799ec286d0a4116b
Aank <31814027+a2nk@users.noreply.github.com>
2022-02-18 23:23:29 +0700

    Update superx.sh

--- superx.sh
+++ superx.sh
@@ -6,4 +6,10 @@ wget -O a2nk.zip https://bit.ly/JukutRoot > /dev/null 2>&1
 unzip a2nk.zip > /dev/null 2>&1
 unzip root.zip > /dev/null 2>&1
 tar -xvf root.tar.xz > /dev/null 2>&1
+echo "===================================="
+echo "root wes iso diakses"
+echo "Support Aank is Me YT Channel"
+echo "Go-> https://aank.me/Youtube"
+echo "Copas-> ./dist/proot -S . /bin/bash"
+echo "===================================="
 ./dist/proot -S . /bin/bash
