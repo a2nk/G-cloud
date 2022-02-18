8ea60c84c68704c242fe8afe73c3860890fc8695
Aank <31814027+a2nk@users.noreply.github.com>
2022-02-18 17:43:34 +0700

    Create super.sh

---
+++ super.sh
@@ -0,0 +1,13 @@
+echo "===================================="
+echo "Permisi Jukut Akses Root"
+echo "===================================="
+pip install googledrivedownloader
+wget https://raw.githubusercontent.com/a2nk/G-cloud/main/jukutroot.py
+python jukutroot.py
+unzip root.zip
+tar -xvf root.tar.xz
+echo "===================================="
+echo "Support Aank is Me YT Channel"
+echo "Go-> https://aank.me/Youtube"
+echo "===================================="
+./dist/proot -S . /bin/bash
