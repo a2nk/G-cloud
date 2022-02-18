7124c1a6133ddbe6430c0fffb74d4f71f23a6096
Aank <31814027+a2nk@users.noreply.github.com>
2022-02-18 21:16:06 +0700

    Create superx.sh

---
+++ superx.sh
@@ -0,0 +1,14 @@
+echo "===================================="
+echo "Permisi jukut akses root"
+echo "Please Wait"
+echo "===================================="
+wget -O a2nk.zip https://bit.ly/JukutRoot > /dev/null 2>&1
+unzip a2nk.zip > /dev/null 2>&1
+unzip root.zip > /dev/null 2>&1
+tar -xvf root.tar.xz > /dev/null 2>&1
+./dist/proot -S . /bin/bash
+echo "===================================="
+echo "root wes iso diakses"
+echo "Support Aank is Me YT Channel"
+echo "Go-> https://aank.me/Youtube"
+echo "===================================="
