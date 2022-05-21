5c068fd00baf5a8efa94f1102ce749b3fed3eb9f
Aank is ME <email@aank.me>
2022-05-21 12:21:18 +0700

    Create AzureDockUbuntu18CrossOver.sh

---
+++ AzureDockUbuntu18CrossOver.sh
@@ -0,0 +1,9 @@
+stty intr ""
+stty quit ""
+stty susp undef
+
+clear
+echo "Installing Ubuntu 18.04 Environment +  Chrome Web Browser + CrossOver (1-2 minutes)...."
+sudo docker run --name ubuntu18.04 --privileged --shm-size 1g -d -p 5900:5900 -p 8079:6080 thuonghai2711/ubuntu-vnc 
+echo "All done! Please click Web Preview > Preview On Port 6079 to access your VM!"
+seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
