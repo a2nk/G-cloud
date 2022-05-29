66c647e2177cff94b69e24982731bef378168a9a
Aank is ME <email@aank.me>
2022-05-29 08:21:14 +0700

    Update DockUbuntu18CrossOver.sh

--- DockUbuntu18CrossOver.sh
+++ DockUbuntu18CrossOver.sh
@@ -1,9 +1,10 @@
-stty intr ""
-stty quit ""
-stty susp undef
-
-clear
-echo "Installing Ubuntu 18.04 Environment +  Chrome Web Browser + CrossOver (1-2 minutes)...."
+# Free Google novnc
+#!/bin/sh
+AankLG='\033[1;32m'
+NC='\033[0m'
+sudo apt install screen
+screen -R novnc
+echo "Installing Ubuntu 18.04 ${AankLG}Environment +  Chrome Web Browser + CrossOver${NC} (1-2 minutes)...."
 sudo docker run --name ubuntu18.04 --privileged --shm-size 1g -d -p 5900:5900 -p 8080:6080 aank999/ubuntu-vnc 
-echo "All done! Please click Web Preview > Preview On Port 8080 to access your VM!"
+echo "All done! Please click Web Preview > Preview On ${AankLG}Port 8080${NC} to access your VM!"
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
