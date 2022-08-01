4ab88ae2ac9379205f80ccb67f2f5f22001f8598
Aank is ME <email@aank.me>
2022-08-01 17:26:21 +0700

    Update xrdp-kali.sh

--- xrdp-kali.sh
+++ xrdp-kali.sh
@@ -18,17 +18,17 @@ echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 3388 &>/dev/null &
 echo "===================================="
-echo "Install RDP"
+echo "Install XRDP Kali Linux"
 echo "===================================="
 docker pull danielguerra/ubuntu-xrdp:kali
 clear
 echo "===================================="
-echo "Start XRDP"
+echo "Start XRDP Kali Linux"
 echo "===================================="
 echo "===================================="
 echo "Username : ubuntu"
 echo "Password : ubuntu"
-echo "RDP Address:"
+echo "XRDP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "IP:" && curl --silent --show-error ipconfig.io
 echo "===================================="
